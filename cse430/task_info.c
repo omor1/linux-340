06f561e7b1cf39bcfa27981ac9475c08897db87b
Omri Mor <omor1@asu.edu>
2016-02-08 21:02:38 -0700

    Optimize sys_task_info()

--- cse430/task_info.c
+++ cse430/task_info.c
@@ -1,6 +1,5 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
-#include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/tty.h>
 #include <linux/uaccess.h>
@@ -20,7 +19,8 @@ asmlinkage long sys_ntasks(size_t __user *num)
 asmlinkage long sys_task_info(struct task_info __user *buf, size_t len)
 {
 	long ret = 0;
-	struct task_info *kbuf = kmalloc_array(len, sizeof(*kbuf), GFP_KERNEL);
+	struct task_info ktask_info;
+	cputime_t utime, stime;
 	size_t i = 0;
 	struct task_struct *task;
 	for_each_process(task) {
@@ -28,17 +28,15 @@ asmlinkage long sys_task_info(struct task_info __user *buf, size_t len)
 			ret = -ENOBUFS;
 			break;
 		}
-		kbuf[i].pid = task_pid_nr(task);
-		tty_name(task->signal->tty, kbuf[i].tty_name);
-		cputime_t utime, stime;
+		ktask_info.pid = task_pid_nr(task);
+		tty_name(task->signal->tty, ktask_info.tty_name);
 		thread_group_cputime_adjusted(task, &utime, &stime);
-		kbuf[i].nsecs = cputime_to_nsecs(utime + stime);
-		get_task_comm(kbuf[i].comm, task);
+		ktask_info.nsecs = cputime_to_nsecs(utime + stime);
+		get_task_comm(ktask_info.comm, task);
+		copy_to_user(&buf[i], &ktask_info, sizeof(*buf));
 		i++;
 	}
 	if (i < len)
 		ret = -E2BIG;
-	copy_to_user(buf, kbuf, sizeof(*buf) * len);
-	kfree(kbuf);
 	return ret;
 }
